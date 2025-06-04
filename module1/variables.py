6152b10cf2909a463f8accb7b9404cc4dea74bc2
Student <blerdonibriqi33@gmail.com>
2025-06-04 17:22:37 +0200

    module_1

---
+++ module1/variables.py
@@ -0,0 +1,30 @@
+temperature = 13
+name = ("Blerdon")
+#print(temperature)
+#print(name)
+print(type(temperature))
+print(type(name))
+
+
+x=10
+y=20
+#print(x+y)
+z=x+y
+
+
+
+
+name1 = "Blerdon"
+surname = "Ibriqi"
+print(name1 + surname)
+
+
+
+
+
+
+
+
+
+
+
